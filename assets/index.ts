82cf9352cb49ea1096d44a29db1cc194186d4030
Sebas -_- <wsm.naranjo@yavirac.edu.ec>
2025-08-01 09:33:12 -0500

    Update index.ts

--- assets/index.ts
+++ assets/index.ts
@@ -1,6 +1,6 @@
 // Centralizar todas las referencias a assets
 export const images = {
-  escudoFuerzaAerea: require('./images/Escudo_Fuerza_Aerea_Ecuador.png'),
+  escudoFuerzaAerea: require("./images/Escudo_Fuerza_Aerea_Ecuador.png"),
 };
 
 // Exportar por defecto para fácil importación
